6cee58023fff8afae4510964a5fbc3779eb634ad
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-01-29 22:09:04 +0100

    Répétition du mot de passe si non valide

--- password.py
+++ password.py
@@ -1,70 +1,70 @@
 import string
 
 # 1. Demander à l'utilisateur de choisir un mot de passe.
-print("Veuillez choisir un mot de passe:")
-password = input("")
+def ask_user_pwd():
+    print("Veuillez choisir un mot de passe:")
+    global password
+    password = input("")
+    return True
+
+ask_user_pwd()
 
 # Il doit contenir au moins 8 caractères.
-def length_pwd(password):
+def length_pwd():
+    global password
     while True:
         if len(password) < 8:
-            print("Le mot de passe contient moins de 8 caractères.")
-            password = input()
+            ask_user_pwd()
         if len(password) >= 8:
             return True
 
-length_pwd(password)
-
 # Il doit contenir au moins une lettre majuscule.
-def uppercase_pwd(password):
+def uppercase_pwd():
     uppercase = any(i.isupper() for i in password)
     if uppercase:
         return True
 
-uppercase_pwd(password)
-
 # Il doit contenir au moins une lettre minuscule.
-def lowercase_pwd(password):
+def lowercase_pwd():
     lowercase = any(i.islower() for i in password)
     if lowercase:
         return True
 
-lowercase_pwd(password)
-
 # Il doit contenir au moins un chiffre.
-def number_pwd(password):
+def number_pwd():
     number = any(i.isdigit() for i in password)
     if number:
         return True
 
-number_pwd(password)
-
 # Il doit contenir au moins un caractère spécial (!, @, #, $, %, ^, &, *).
-def spe_char_pwd(password):
+def spe_char_pwd():
     spe_char = string.punctuation
     if any(i in spe_char for i in password):
         return True
 
-spe_char_pwd(password)
-
 # 2. Vérifiez si le mot de passe choisi respecte les exigences de sécurité.
 def check_security():
-    if length_pwd(password):
-        if uppercase_pwd(password):
-            if lowercase_pwd(password):
-                if number_pwd(password):
-                    if spe_char_pwd(password):
+    if length_pwd():
+        if uppercase_pwd():
+            if lowercase_pwd():
+                if number_pwd():
+                    if spe_char_pwd():
                         print("Le mot de passe est VALIDE.")
                         return True
                     else:
                         print("Le mot de passe ne contient pas de caractères spéciaux.")
+                        ask_user_pwd()
                 else:
                     print("Le mot de passe ne contient pas de nombre.")
+                    ask_user_pwd()
             else:
                 print("Le mot de passe ne contient pas de minuscule.")
+                ask_user_pwd()
         else:
             print("Le mot de passe ne contient pas de majuscule.")
+            ask_user_pwd()
     else:
         print("Le mot de passe est trop court.")
+        ask_user_pwd()
 
 check_security()
